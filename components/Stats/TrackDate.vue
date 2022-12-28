81c399eb0aa3e41a6ab6ee574a9e3367a6b3fc87
metkm <metinkorkmaz417@gmail.com>
2022-12-28 13:03:50 +0300

    add percentage bars

--- components/Stats/TrackDate.vue
+++ components/Stats/TrackDate.vue
@@ -11,13 +11,20 @@ defineProps<{
     <p>total play time {{ dates.totalMsPlayed }}</p>
     
     <div class="rounded" v-for="[key, year] in Object.entries(dates.years)">
-      <p>{{ key }} 
+      <p>Year - {{ key }} 
         <span class="text-xs">total playtime of this year {{ year.totalMsPlayed }}</span>
+
+        <div 
+          class="h-0.5 bg-green-500"
+          :style="{
+            width: `${year.totalMsPlayed / dates.totalMsPlayed * 100}%`
+          }"
+        />
       </p>
 
-      <div class="flex flex-col gap-5 p-4 rounded border">
+      <div class="p-4 rounded border">
         <div v-for="[key, month] in Object.entries(year.months)">
-          <p>{{ key }}
+          <p>Month - {{ key }}
             <span class="text-xs">total playtime of this month {{ month.totalMsPlayed }}</span>
           </p>
 
@@ -25,7 +32,15 @@ defineProps<{
             <p v-for="[key, totalPlayed] in Object.entries(month.days)">
               {{ key }}
               <span class="text-xs">total playtime of this day {{ totalPlayed }}</span>
+
+              <div 
+                class="h-0.5 bg-green-500"
+                :style="{
+                  width: `${totalPlayed / month.totalMsPlayed}%`
+                }"
+              />
             </p>
+
           </div>
         </div>
       </div>
