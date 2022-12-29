a417e8cabcd74e655f9392e0a5a6e14f60d4ea0c
metkm <metinkorkmaz417@gmail.com>
2022-12-29 17:16:15 +0300

    cleanup to most function

--- utils/most.ts
+++ utils/most.ts
@@ -5,35 +5,28 @@ export const mostFrequentSong = (
 ) => {
   if (songs.length < 1) return [];
 
-  let seen: Record<string, {
-    song: Song,
-    seenCount: number
-  }> = {};
+  let newSongList: Song[] = [];
+  let seen: Record<string, number> = {};
 
   for (let index = 0; index < songs.length; index++) {
     const song = songs[index];
 
     if (!song.master_metadata_track_name) continue;
 
-    if (!seen[song.master_metadata_track_name]) {
-      seen[song.master_metadata_track_name] = {
-        song,
-        seenCount: 0
-      }
+    if (!seen.hasOwnProperty(song.master_metadata_track_name)) {
+      seen[song.master_metadata_track_name] = 0;
+      newSongList.push(song);
     } else {
-      seen[song.master_metadata_track_name].seenCount += 1;
+      seen[song.master_metadata_track_name] += 1;
     }
   }
 
-  songs.sort((firstSong, secondSong) => {
-    let firstSongSeenCount = seen[firstSong.master_metadata_track_name!].seenCount;
-    let secondSongSeenCount = seen[secondSong.master_metadata_track_name!].seenCount;
+  newSongList.sort((firstSong, secondSong) => {
+    let firstSongSeenCount = seen[firstSong.master_metadata_track_name!];
+    let secondSongSeenCount = seen[secondSong.master_metadata_track_name!];
 
     return secondSongSeenCount - firstSongSeenCount;
   });
 
-  return Object.values(seen)
-    .map(value => {
-      return value.song
-    })
+  return newSongList;
 }
