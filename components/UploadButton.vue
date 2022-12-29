7461d3c28d835f7b32910976eb4cc7a64c867bf9
metkm <metinkorkmaz417@gmail.com>
2022-12-29 13:29:15 +0300

    show current status

--- components/UploadButton.vue
+++ components/UploadButton.vue
@@ -1,3 +1,9 @@
+<script lang="ts">
+export default {
+  inheritAttrs: false
+}
+</script>
+
 <script setup lang="ts">
 import { WrappedResult } from '~~/models/Song';
 
@@ -34,47 +40,18 @@ const upload = () => {
   const inputElement = openFilePicker();
   inputElement.addEventListener("change", fileSelectHandler);
 }
-
-// const readFileAsync = async (file: File): Promise<string> => {
-//   return new Promise(resolve => {
-//     let fileReader = new FileReader();
-  
-//     fileReader.addEventListener("load", () => {
-//       resolve(fileReader.result as string)
-//     })
-  
-//     fileReader.readAsText(file);
-//   });
-// }
-
-// const fileSelectHandler = async (event: Event) => {
-//   const element = event.target as HTMLInputElement;
-
-//   if (!element.files) {
-//     return;
-//   }
-
-//   let contents: Song[] = [];
-//   for (const file of element.files) {
-//     console.log("test");
-//     let content = await readFileAsync(file);
-//     contents.push(...JSON.parse(content));
-//   }
-
-//   emit("update:modelValue", contents);
-// }
-
-// const upload = () => {
-//   const element = openFilePicker();
-//   element.addEventListener("change", fileSelectHandler);
-// }
 </script>
 
 <template>
-  <button
-    @click="upload"
-    class="bg-green-500 p-2 shadow-lg text-white hover:bg-red-500"
-  >
-    Upload
-  </button>
+  <div class="flex flex-col">
+    <button
+      @click="upload"
+      class="bg-green-500 p-2 shadow-lg text-white hover:bg-red-500" 
+      v-bind="$attrs"
+    >
+      Upload
+    </button>
+
+    <p>{{ currentStatus }}</p>
+  </div>
 </template>
