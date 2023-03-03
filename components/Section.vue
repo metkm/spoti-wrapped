f64af4f6f223f290adac1a8cc5b058108b6dad0d
metkm <metinkorkmaz417@gmail.com>
2023-03-03 13:22:49 +0300

    update section title to non bold font

--- components/Section.vue
+++ components/Section.vue
@@ -8,7 +8,7 @@ defineProps<{
 <template>
   <div class="rounded-lg border shadow-log relative">
     <div class="p-4 text-center">
-      <p class="font-bold text-xl">{{ title }}</p>
+      <p class="text-xl">{{ title }}</p>
       <p class="text-sm text-neutral-400">{{ desc }}</p>
     </div>
     
