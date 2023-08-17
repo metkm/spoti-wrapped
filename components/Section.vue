62e555b84fd9fb9914ede46138c313749dfaf09f
metkm <metinkorkmaz417@gmail.com>
2023-08-17 18:27:10 +0300

    add padding to section titles

--- components/Section.vue
+++ components/Section.vue
@@ -7,7 +7,7 @@ defineProps<{
 
 <template>
   <section class="rounded-lg border shadow-log relative">
-    <div class="text-center">
+    <div class="text-center p-4">
       <h1 class="text-xl">{{ title }}</h1>
       <p v-if="desc" class="text-sm text-neutral-400">{{ desc }}</p>
     </div>
