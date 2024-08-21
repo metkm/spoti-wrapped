895653f3ee941762b2c6db955f34e2fab8a25872
metkm <metinkorkmaz417@gmail.com>
2024-08-21 14:24:32 +0300

    remove unused skeletons

--- app/components/skeleton/History.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="grid grid-cols-3 gap-4 grid-rows-3 size-[30rem]">
-    <div
-      v-for="i in 3 * 3"
-      :key="i"
-      class="animate-pulse bg-neutral-800/50 rounded-lg"
-    />
-  </div>
-</template>
