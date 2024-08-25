4022cc98ce0ed8d245b9178f108d0ed32fdb9b76
metkm <metinkorkmaz417@gmail.com>
2024-08-25 20:46:54 +0300

    remove base error because It's buggy

--- app/components/Playlist.vue
+++ app/components/Playlist.vue
@@ -22,31 +22,29 @@ defineProps<{
     <div class="absolute inset-0 bg-black/40" />
 
     <div class="flex flex-col gap-4 z-10 h-full w-full text-white">
-      <BaseError>
-        <Transition
-          enter-active-class="transition-all duration-300"
-          leave-active-class="transition-all duration-300"
-          enter-from-class="opacity-0"
-          leave-to-class="opacity-0"
-          mode="out-in"
-        >
-          <PlaylistTracks
-            v-if="selected"
-            :id="playlist.id"
-            :counts
-            class="backdrop-blur-[200px]"
-          />
+      <Transition
+        enter-active-class="transition-all duration-300"
+        leave-active-class="transition-all duration-300"
+        enter-from-class="opacity-0"
+        leave-to-class="opacity-0"
+        mode="out-in"
+      >
+        <PlaylistTracks
+          v-if="selected"
+          :id="playlist.id"
+          :counts
+          class="backdrop-blur-[200px]"
+        />
 
-          <div
-            v-else
-            class="p-4"
-          >
-            <p class="font-semibold text-left text-2xl">
-              {{ playlist.name }}
-            </p>
-          </div>
-        </Transition>
-      </BaseError>
+        <div
+          v-else
+          class="p-4"
+        >
+          <p class="font-semibold text-left text-2xl">
+            {{ playlist.name }}
+          </p>
+        </div>
+      </Transition>
     </div>
   </div>
 </template>
