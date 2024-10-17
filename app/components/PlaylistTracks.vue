1274cf8ddcc8fd602a51a3c87ebf1ddb070c7c49
metkm <metinkorkmaz417@gmail.com>
2024-10-17 20:28:09 +0300

    wip

--- app/components/PlaylistTracks.vue
+++ app/components/PlaylistTracks.vue
@@ -11,6 +11,36 @@ const props = defineProps<{
 const query = ref('')
 const queryDebounced = refDebounced(query, 1000)
 
+const container = useTemplateRef('container')
+
+const offset = ref(0)
+const page = ref(1)
+const trackItems = ref<TrackItem[]>([])
+
+// makes request 3 times wtf
+const { data, status, refresh, execute } = await useSpotifyFetch<Pagination<TrackItem>>(`/playlists/${props.id}/tracks`, {
+  key: `playlist:${props.id}`,
+  params: { offset: offset },
+  onResponse: (response) => {
+    trackItems.value.push(...response.response._data.items)
+    console.log('response')
+
+    return response
+  },
+  watch: false,
+})
+
+useInfiniteScroll(container, () => {
+  offset.value += 20 * page.value
+  page.value++
+}, {
+  distance: 20,
+  throttle: 500,
+  canLoadMore: () => status.value !== 'pending',
+})
+
+watch(page, () => execute())
+
 // const container = ref<HTMLElement>()
 // const trackItems = ref<TrackItem[]>([])
 // const page = ref(1)
@@ -71,13 +101,13 @@ const queryDebounced = refDebounced(query, 1000)
 <template>
   <div class="flex flex-col gap-2 overflow-hidden h-full p-2 rounded-lg">
     <div class="flex items-center gap-2 z-10">
-      <!-- <UButton
+      <UButton
         leading-icon="i-heroicons-arrow-path-16-solid"
         :loading="status === 'pending'"
         square
         variant="ghost"
-        @click="refresh"
-      /> -->
+        @click="() => refresh()"
+      />
       <UInput
         v-model="query"
         placeholder="Search"
@@ -85,6 +115,62 @@ const queryDebounced = refDebounced(query, 1000)
         class="grow"
       />
     </div>
+
+    <ul
+      ref="container"
+      class="h-full overflow-y-auto overflow-fade"
+    >
+      <li
+        v-for="({ track }, index) in trackItems"
+        :key="track.id"
+        class="flex items-center gap-2"
+      >
+        <p>{{ index + 1 }}</p>
+
+        <div class="flex items-center gap-2 p-2">
+          <img
+            :src="track.album.images.at(0)?.url"
+            class="size-14 rounded"
+          >
+
+          <div class="text-left">
+            <p>
+              {{ track.name }}
+            </p>
+
+            <ul
+              class="flex items-center flex-wrap gap-6 gap-y-0 text-xs text-neutral-400 list-disc"
+            >
+              <li v-if="counts.skip[track.id] && counts.skip[track.id]! > 0">
+                <p>Skip count {{ counts.skip[track.id] }}</p>
+              </li>
+
+              <li
+                v-if="counts.listen[track.id] && counts.listen[track.id]! > 0"
+              >
+                <p>Listen count {{ counts.listen[track.id] }}</p>
+              </li>
+
+              <li
+                v-if="counts.totalMsPlayed[track.id] && counts.totalMsPlayed[track.id]! > 0"
+              >
+                <p>
+                  Average of
+                  {{
+                    Math.round(
+                      counts.totalMsPlayed[track.id]!
+                        / 1000
+                        / counts.listen[track.id]!,
+                    )
+                  }}
+                  seconds listened
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </li>
+    </ul>
 
     <!-- <ul
       ref="container"
